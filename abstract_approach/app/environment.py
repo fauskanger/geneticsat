ba693a3d4032b29e1c401d9d650079aa64d5488c
Thomas Fauskanger <thomasfauskanger@gmail.com>
2015-09-02 13:21:52 +0200

    Added safe get of clauses, refactor: conjunction -> clause

--- abstract_approach/app/environment.py
+++ abstract_approach/app/environment.py
@@ -3,10 +3,15 @@
 
 
 class Environment(object):
-    def __init__(self):
-        pass
+    def __init__(self, accepted_chromosome_sizes=None):
+        default_szs = [8]
+        szs = accepted_chromosome_sizes
+        self._chromosome_accepted_sizes = szs if szs else default_szs
 
-    def get_conjunctions(self, chromosome):
+    def _verify_chromosome(self, chromosome):
+        return chromosome.length() in self._chromosome_accepted_sizes
+
+    def get_clauses(self, chromosome):
         x1, x2, x3, x4, x5, x6, x7, x8 = chromosome.genes()
         return [
             x1 or x5,
@@ -18,16 +23,19 @@ class Environment(object):
         ]
 
     def chromosome_fitness(self, chromosome):
-        conjunctions = self.get_conjunctions(chromosome)
+        conjunctions = self._safe_get_clauses(chromosome)
         fitness = sum(1 for premise in conjunctions if premise)
         return fitness/len(conjunctions)
+
+    def _safe_get_clauses(self, chromosome):
+        return [] if not self._verify_chromosome(chromosome) else self.get_clauses(chromosome)
 
 
 class DifficultEnvironment(Environment):
     def __init__(self):
         super().__init__()
 
-    def get_conjunctions(self, chromosome):
+    def get_clauses(self, chromosome):
         x1, x2, x3, x4, x5, x6, x7, x8 = chromosome.genes()
         return [
             x1 or x5,
